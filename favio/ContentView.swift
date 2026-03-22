e8ed89ccbb8950cf995c52f41000848081862b62
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-03-22 21:19:20 +0200

    Initial Commit

---
+++ favio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  favio
+//
+//  Created by Yaroslav Sorochan on 22.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ favio/favioApp.swift
@@ -0,0 +1,17 @@
+//
+//  favioApp.swift
+//  favio
+//
+//  Created by Yaroslav Sorochan on 22.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct favioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
